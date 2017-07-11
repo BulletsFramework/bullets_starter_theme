3bc71b962604bf85c9267754cdb3088903ecba45
Joe <joe@neptik.com>
2017-07-11 12:19:27 +0000

    Removed '<div>' from page-modules.php

--- parts/page-modules.php
+++ parts/page-modules.php
@@ -2,20 +2,16 @@
 	// check if the flexible content field has rows of data
 	if( have_rows('page_modules') ):
 	    $page_modules = true;
-
-	    echo "<div class='modules paddingends--half'>";
 	    
 	    // loop through the rows of data
 	    while ( have_rows('page_modules') ) : the_row();
 
-	        if( get_row_layout() == 'single_box_content_area'  ) {
+	        if( get_row_layout() == 'content_with_image'  ) {
 	        	include(locate_template('parts/modules/single-box-content-area.php'));
 
 	        }   
 
 	    endwhile;
-
-	    echo "</div>";
 
 	else :
 
